9e2975a675e9c6b0e43c48465b64bcab1eff5a7e
Alenjs <alen.brasil@gmail.com>
2020-09-05 23:02:17 -0300

    Criando e inserindo no Banco de Dados

---
+++ src/database/createProffy.js
@@ -0,0 +1,21 @@
+module.exports = async function (db, { proffyValue, ClassValue, classScheduleValue }){
+    //inserir dados na tabela de proffys
+    const insertedProffy = await db.run(`
+        INSERT INTO proffys (
+            name,
+            avatar,
+            whatsapp,
+            bio
+        ) VALUES (
+            ${proffyValue.name}, 
+            ${proffyValue.avatar},
+            ${proffyValue.whatsapp},
+            ${proffyValue.bio}
+        );
+    `)
+
+    const proffy_id = insertedProffy.lastID
+
+    await db.run()
+    await db.run()
+}
